a672ae0835d568effa915987aa315d7245a4642a
mellowcellofellow <161681469+mellowcellofellow@users.noreply.github.com>
2024-03-20 22:06:26 -0600

    Updated after our meeting today.

--- main.ts
+++ main.ts
@@ -53,7 +53,7 @@ function DrumLevel1 () {
     QuarterNote()
 }
 function QuarterNote () {
-    Quarter = sprites.create(assets.image`WholePic`, SpriteKind.Projectile)
+    Quarter = sprites.create(assets.image`QuarterPic`, SpriteKind.Projectile)
     Quarter.setPosition(161, 108)
     Quarter.setVelocity(-50, 0)
     pause(520)
@@ -70,32 +70,12 @@ function BassClass () {
     tiles.setCurrentTilemap(tilemap`level3`)
     scene.setBackgroundImage(assets.image`Bass Room`)
     scene.centerCameraAt(0, 0)
-    mySprite2 = sprites.create(img`
-        f 
-        `, SpriteKind.Press)
-    mySprite = sprites.create(img`
-        . . . . f f f f . . . . . 
-        . . f f f f f f f f . . . 
-        . f f f f f f c f f f . . 
-        f f f f f f c c f f f c . 
-        f f f c f f f f f f f c . 
-        c c c f f f e e f f c c . 
-        f f f f f e e f f c c f . 
-        f f f b f e e f b f f f . 
-        . f 4 1 f 4 4 f 1 4 f . . 
-        . f e 4 4 4 4 4 4 e f . . 
-        . f f f e e e e f f f . . 
-        f e f b 7 7 7 7 b f e f . 
-        e 4 f 7 7 7 7 7 7 f 4 e . 
-        e e f 6 6 6 6 6 6 f e e . 
-        . . . f f f f f f . . . . 
-        . . . f f . . f f . . . . 
-        `, SpriteKind.Player)
+    mySprite2 = sprites.create(assets.image`Point`, SpriteKind.Press)
+    mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
     mySprite.setPosition(21, 108)
     mySprite2.setPosition(21, 112)
     controller.moveSprite(mySprite, 0, 0)
     game.splash("Get Ready!")
-    DrumLevel1()
 }
 function Load_School_View () {
     scene.setBackgroundImage(img`
@@ -221,24 +201,7 @@ function Load_School_View () {
         ................................................................................................................................................................
         `)
     tiles.setCurrentTilemap(tilemap`School View Tilemap`)
-    mySprite = sprites.create(img`
-        . . . . f f f f . . . . . 
-        . . f f f f f f f f . . . 
-        . f f f f f f c f f f . . 
-        f f f f f f c c f f f c . 
-        f f f c f f f f f f f c . 
-        c c c f f f e e f f c c . 
-        f f f f f e e f f c c f . 
-        f f f b f e e f b f f f . 
-        . f 4 1 f 4 4 f 1 4 f . . 
-        . f e 4 4 4 4 4 4 e f . . 
-        . f f f e e e e f f f . . 
-        f e f b 7 7 7 7 b f e f . 
-        e 4 f 7 7 7 7 7 7 f 4 e . 
-        e e f 6 6 6 6 6 6 f e e . 
-        . . . f f f f f f . . . . 
-        . . . f f . . f f . . . . 
-        `, SpriteKind.Player)
+    mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
     controller.moveSprite(mySprite)
     scene.cameraFollowSprite(mySprite)
     tiles.placeOnTile(mySprite, tiles.getTileLocation(11, 23))
@@ -259,27 +222,8 @@ function DrumClass () {
     tiles.setCurrentTilemap(tilemap`level3`)
     scene.setBackgroundImage(assets.image`Bass Room`)
     scene.centerCameraAt(0, 0)
-    mySprite2 = sprites.create(img`
-        f 
-        `, SpriteKind.Press)
-    mySprite = sprites.create(img`
-        . . . . f f f f . . . . . 
-        . . f f f f f f f f . . . 
-        . f f f f f f c f f f . . 
-        f f f f f f c c f f f c . 
-        f f f c f f f f f f f c . 
-        c c c f f f e e f f c c . 
-        f f f f f e e f f c c f . 
-        f f f b f e e f b f f f . 
-        . f 4 1 f 4 4 f 1 4 f . . 
-        . f e 4 4 4 4 4 4 e f . . 
-        . f f f e e e e f f f . . 
-        f e f b 7 7 7 7 b f e f . 
-        e 4 f 7 7 7 7 7 7 f 4 e . 
-        e e f 6 6 6 6 6 6 f e e . 
-        . . . f f f f f f . . . . 
-        . . . f f . . f f . . . . 
-        `, SpriteKind.Player)
+    mySprite2 = sprites.create(assets.image`Point`, SpriteKind.Press)
+    mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
     mySprite.setPosition(21, 108)
     mySprite2.setPosition(21, 112)
     controller.moveSprite(mySprite, 0, 0)
