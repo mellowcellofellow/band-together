a42777ba96837944c9fda2d14c49ef5d7aacabda
kenpeterson2112 <164274388+kenpeterson2112@users.noreply.github.com>
2024-03-24 19:18:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -68,10 +68,8 @@ function QuarterNote () {
 }
 function Load_Intro_Sequence () {
     scene.setBackgroundImage(assets.image`School Front`)
-    game.showLongText("Hey PLAYER! Starting at a new School can be tough. I wonder if you will be able to meet anyone cool?", DialogLayout.Bottom)
-    game.showLongText("What's this? A poster? For a Battle of the Bands?", DialogLayout.Bottom)
-    game.showLongText("\"Battle this Friday, all bands should have a singer, guitar, bass and drums.\"", DialogLayout.Bottom)
-    game.showLongText("That sounds like a way to meet people. I wonder where I'm going to find people to join my band?", DialogLayout.Bottom)
+    game.showLongText("Hey PLAYER! Starting at a new School can be tough. How are you going to fit in?", DialogLayout.Bottom)
+    game.showLongText("Press A to enter your new school.", DialogLayout.Bottom)
 }
 function BassClass () {
     tiles.setCurrentTilemap(tilemap`level3`)
